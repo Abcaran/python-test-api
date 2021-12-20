74ad8b2f812c4132de5dce8d2dfe0dbb0c377ab5
abcaran <samirsaadi@gmail.com>
2021-12-20 20:21:59 -0300

    start testing

--- tests/views/test_cpf.py
+++ tests/views/test_cpf.py
@@ -1,13 +1,19 @@
-BASE = 'http://127.0.0.1:5000/'
+import unittest
+
+import main
 
 
-class CPFViewTestCase:
+class CPFViewTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.app = main.app
+        self.response = self.app.get('/<string:cpf>/')
 
     def test_get_empty_params(self):
         pass
 
     def test_get_cpf_allowed(self):
-        pass
+        self.assertEqual(302, self.response.status_code)
 
     def test_get_cpf_blacklisted(self):
         pass
